32535007158a27d7b7c9aa6b4b1c0bab0ca1ea63
Shizcobob <kyle.korinek@outlook.com>
2023-07-13 12:01:30 -0400

    Submission

--- algo.js
+++ algo.js
@@ -28,16 +28,49 @@ const expected3 = [
 function allNonConsecutive(sortedNumbers) {
 
     for(let j = 0; j < sortedNumbers.length; j++){
-        if(sortedNumbers[j+1] != sortedNumbers[j]+1 || sortedNumbers[j+1] == undefined){
+        if(sortedNumbers[j+1] != sortedNumbers[j]+1){
             let n = {}
 
-            n.sortedNumbers[[j]]
-            i.sortedNumbers[j]
             b.push({i,n});
         }
     }
 }
 console.log(allNonConsecutive(numbers1))
 console.log(allNonConsecutive(numbers2))
+
+
+// Working example
+
+// Explanation: Index 0 has no number before it, so it is not included.
+
+/**
+ 
+Finds all the non-consecutive (out of order) numbers from the given array.
+Time: O(?).
+Space: O(?).
+@param {Array<number>} sortedNumbers
+@typedef {Array<{i: number, n: number}>} NonConsecutiveNumbers Array of objects.
+@property {number} i The index of the non consecutive number.
+@property {number} n The non consecutive number itself.
+@returns {NonConsecutiveNumbers}
+*/
+function allNonConsecutive(sortedNumbers) {
+    let result = [] //declaring an empty result
+    if (sortedNumbers.length < 2) { //check if the initial array is too short to connect.
+        return result 
+    }
+
+    for (let index = 1; index < sortedNumbers.length; index++) { // iterating through the numbers.
+        if (sortedNumbers[index] != ( sortedNumbers[index - 1] + 1 ) ) { // to check if the current value is one greater than the previous value.
+            let temp = { i: index, n: sortedNumbers[index] } // formatting new information.
+            result.push(temp) // pushing new info into result
+        }
+    }
+    return result 
+}
+console.log(allNonConsecutive(numbers1))
+console.log(allNonConsecutive(numbers2))
+console.log(allNonConsecutive(numbers3))
+
 
 /*****************************************************************************/
